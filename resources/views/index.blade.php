570d6c53c20abf6f7bcc6275dedd3428cdc1b728
c3budiman <c3budiman@gmail.com>
2018-01-17 14:20:37 +0700

    starting

---
+++ resources/views/index.blade.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Pusat Studi Desain Otomotif</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- CSS
+        ================================================ -->
+        <!-- Owl Carousel -->
+		<link rel="stylesheet" href="css/owl.carousel.css">
+        <!-- bootstrap.min css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Font-awesome.min css -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- Main Stylesheet -->
+		<link rel="stylesheet" href="css/animate.min.css">
+
+		<link rel="stylesheet" href="css/main.css">
+        <!-- Responsive Stylesheet -->
+		<link rel="stylesheet" href="css/responsive.css">
+		<!-- Js -->
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="js/jquery.nav.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/main.js"></script>
+	</head>
+	<body>
+	<!--
+	header-img start
+	============================== -->
+    <section id="hero-area">
+      <img class="img-responsive" src="images/header.jpg" alt="">
+    </section>
+	<!--
+    Header start
+	============================== -->
+	<nav id="navigation">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block">
+                        <nav class="navbar navbar-default">
+                          <div class="container-fluid">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                              </button>
+                                  <a class="navbar-brand" href="#">
+                                   <h3>Pusat Studi Desain_Otomotif</h3>
+                                  </a>
+
+                            </div>
+
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav navbar-right" id="top-nav">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/profil">Profil</a></li>
+                                <li><a href="/tata-tertib">Berita</a></li>
+                                <li><a href="/jadwal">Publikasi</a></li>
+                                <li><a href="/berita">Kegiatan</a></li>
+                                <li><a href="/kelulusan">Riset</a></li>
+                                <li><a href="/rekrut">Kerjasama</a></li>
+																<li><a href="/rekrut">Forum</a></li>
+																<li><a href="/rekrut">Konsultasi</a></li>
+																<li><a href="/rekrut">Video</a></li>
+                              </ul>
+                            </div><!-- /.navbar-collapse -->
+                          </div><!-- /.container-fluid -->
+                        </nav>
+                    </div>
+                </div><!-- .col-md-12 close -->
+            </div><!-- .row close -->
+        </div><!-- .container close -->
+	</nav><!-- header close -->
+	<!-- Html Style -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<h2 class="heading-line text-center">Tentang Laboratorium Manajemen Dasar</h2>
+				<div class="row mt-50">
+					<div class="col-xs-12">
+						<p style="font-size:20px;">
+							<span class="dropcap style-1">L</span>aboratorium Manajemen Dasar merupakan salah satu Laboratorium di Universitas Gunadarma untuk mahasiswa fakultas Ekonomi. Ruangan staff terletak di E52 dan J1421 sedangkan pelaksanaan praktikum di E532, E533, H402, H408, H4045, C018, K1, L2 dan J1423.
+						</p>
+					</div>
+				</div> <!-- end row -->
+			</div>
+{{--
+		  <div class="col-md-3">
+
+			</div> --}}
+
+			<div class="col-md-4">
+				<form method="post" action="#">
+					<div class="form-group">
+						<div class="input-group input-group-sm">
+							<input placeholder="Input Kelas" type="text" name="kelas" class="form-control" autofocus>
+							{{ csrf_field() }}
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-sm btn-orange btn-icon"><span>Cari Jadwal</span>
+								<i class="fa fa-angle-right"></i></button>
+
+							</span>
+						</div>
+					</div>
+					<div id="form-subscribe-footer" class="form-output"></div>
+				</form>
+
+			</div>
+		</div>
+	</div>
+	<section class="section-wrap pb-0">
+		<div class="container">
+			<br>
+			<div class="row">
+			<h2 class="heading-line text-center">Visi dan Misi</h2>
+				<div class="col-sm-6">
+					<blockquote class="blockquote-style-1 mb-30">
+							<b>Visi : </b>
+							<br>Pada tahun 2022 menjadi laboratorium penyelenggara praktikum yang bereputasi Internasional, memiliki jejaring global, dan memberikan kontribusi signifikan bagi peningkatan daya saing bangsa khususnya bidang Manajemen berbasis IT.
+					</blockquote>
+				</div>
+				<div class="col-sm-6">
+					<blockquote class="blockquote-style-2">
+						<b>Misi : </b>
+						<br> <ol>
+							<li>Melaksanakan praktikum untuk menghasilkan sarjana Manajemen yang profesional dan mampu mengikuti perkembangan ilmu pengetahuan dan teknologi serta mampu bersaing di lingkungan global.</li>
+							<li>Melaksanakan kegiatan pengembangan dalam bidang Manajemen sehingga dapat memberikan kontribusi kepada kemajuan ilmu pengetahuan.</li>
+						</ol>
+					</blockquote>
+				</div>
+			</div>
+		</div> <!-- end container -->
+	</section> <!-- end html style -->
+
+
+	<!-- From Blog -->
+<section class="section-wrap from-blog style-2 pb-90 bg-light">
+	<div class="container">
+
+		<div class="row mb-60">
+			<div class="col-md-12 text-center">
+				<h2>Berita Terbaru</h2>
+				<br>
+			</div>
+		</div>
+
+		<div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.1s">
+			<div class="blog-col">
+				<div class="entry-img">
+					<img src="images/polricat.jpg" alt="">
+				</div>
+				<div class="entry-box">
+					<h4 class="entry-title"><a href="blog-single.html">KERJASAMA UJIAN CAT POLDA DENGAN UNIVERSITAS GUNADARMA</a></h4>
+					<ul class="entry-meta">
+						<li>July 10, 2015</li>
+						<li><a href="blog-single.html">10 Comments</a></li>
+					</ul>
+					<div class="entry-content">
+						<p>Universitas Gunadarma dipilih bekerjasama dengan Polda Metro Jaya untuk memfasilitasi seleksi penerimaan anggota Kepolisian Negara Republik Indonesia ‎(Polri) dengan sistem CAT (Computer Assited Test).</p>
+						<a href="blog-single.html" class="btn btn-sm btn-orange">Baca Selengkapnya</a>
+					</div>
+				</div>
+			</div>
+		</div> <!-- end post -->
+
+		<div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
+			<div class="blog-col">
+				<div class="entry-img">
+					<img src="images/baak.jpg" alt="">
+				</div>
+				<div class="entry-box">
+					<h4 class="entry-title"><a href="blog-single.html" class="dark-link">BATAS AKHIR PENGAMBILAN KRS</a></h4>
+					<ul class="entry-meta">
+						<li>July 10, 2015</li>
+						<li><a href="blog-single.html">10 Comments</a></li>
+					</ul>
+					<div class="entry-content">
+						<p>Dengan ini diberitahukan kepada seluruh mahasiswa Universitas Gunadarma mengenai hal-hal sebagai...</p>
+						<a href="blog-single.html" class="btn btn-sm btn-orange">Baca Selengkapnya</a>
+					</div>
+				</div>
+			</div>
+		</div> <!-- end post -->
+
+		<div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+			<div class="blog-col last">
+				<div class="entry-img">
+					<img src="images/ec.png" alt="">
+				</div>
+				<div class="entry-box">
+					<h4 class="entry-title"><a href="blog-single.html">English Competition 2017</a></h4>
+					<ul class="entry-meta">
+						<li>July 10, 2015</li>
+						<li><a href="blog-single.html">10 Comments</a></li>
+					</ul>
+					<div class="entry-content">
+						<p>Dalam rangka memperingati hari Sumpah Pemuda dan Hari Pahlawan, Fakultas Sastra Universitas Gunadarma akan mengadakakan English Competition, yang akan diadakan pada hari Senin, 13 November 2017, pukul 09.00 s/d 17.00 WIB, di D46. Registrasi mulai pukul 08.00 WIB.</p>
+						<a href="blog-single.html" class="btn btn-sm btn-orange">Baca Selengkapnya</a>
+					</div>
+				</div>
+			</div>
+		</div> <!-- end post -->
+
+	</div>
+</section> <!-- end from blog -->
+
+
+
+
+
+    <!--
+    footer  start
+    ============================= -->
+    <section id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="block wow fadeInLeft"  data-wow-delay="200ms">
+                        <h3>Kontak <span>Info</span></h3>
+                        <div class="info">
+                            <ul>
+                                <li>
+                                  <h4><i class="fa fa-phone"></i>Telephone</h4>
+                                  <p>085770188852(SMS only)</p>
+
+                                </li>
+                                <li>
+                                  <h4><i class="fa fa-map-marker"></i>Alamat</h4>
+                                  <p>Kampus Kelapa Dua E52 - Kampus Kalimalang J1423 (Staff)</p>
+                                </li>
+                                <li>
+                                  <h4><i class="fa fa-envelope"></i>E-mail</h4>
+                                  <p>manajemen.dasar@gmail.com</p>
+																	<p>manajemen_dasar@yahoo.com</p>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- .col-md-4 close -->
+                <div class="col-md-4">
+                    <div class="block wow fadeInLeft"  data-wow-delay="700ms">
+                        <h3>Postingan <span> Blog Terbaru</span></h3>
+                        <div class="blog">
+                            <ul>
+                                <li>
+                                    <h4><a href="#">Kerjasama ujian CAT Polda dengan Universitas Gunadarma</a></h4>
+                                    <p>Universitas Gunadarma dipilih bekerjasama dengan Polda Metro Jaya untuk memfasilitasi seleksi penerimaan anggota Kepolisian Negara Republik Indonesia ‎(Polri) dengan sistem CAT (Computer Assited Test).</p>
+                                </li>
+                                <li>
+                                    <h4><a href="#">Batas Akhir Pengambilan KRS</a></h4>
+                                    <p>Dengan ini diberitahukan kepada seluruh mahasiswa Universitas Gunadarma mengenai hal-hal sebagai berikut :
+                                   </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- .col-md-4 close -->
+                <div class="col-md-4">
+                    <div class="block wow fadeInLeft"  data-wow-delay="1100ms">
+                        <div class="gallary">
+                            <h3>Universitas <span>Gunadarma</span></h3>
+                            <a target="_blank" href="http://gunadarma.ac.id"><img class="img-responsive" src="images/logogundar.png" /></a>
+                        </div>
+                        <div class="social-media-link">
+                            <h3>Follow <span>US</span></h3>
+                            <ul>
+                                <li>
+                                    <a target="_blank" href="http://twitter/labmadas_feug">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a target="_blank" href="http://instagram.com/labmadas_feug">
+                                        <i class="fa fa-instagram"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- .col-md-4 close -->
+            </div><!-- .row close -->
+        </div><!-- .containe close -->
+    </section><!-- #footer close -->
+    <!--
+    footer-bottom  start
+    ============================= -->
+    <footer id="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="block">
+                        <p> The Official Site of Lab Manajemen Dasar <a href="#"></a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+	</body>
+</html>
